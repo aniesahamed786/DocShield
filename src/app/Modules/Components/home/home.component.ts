3d7e159b5fbd066bcf933a129e43f59a17523436
ANIES AHAMED <56381997+aniesahamed786@users.noreply.github.com>
2023-08-24 11:05:03 +0530

    new_changes

--- src/app/Modules/Components/home/home.component.ts
+++ src/app/Modules/Components/home/home.component.ts
@@ -18,4 +18,11 @@ export class HomeComponent {
     {position: 6, name: 'Vengadassalabady, Dinesh', weight: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur', symbol: 'watch_later'},
 
   ];
+  searchText: string = '';
+  filteredItems: any = this.dataSource;
+  onSearchTextChange() {
+    this.filteredItems = this.dataSource.filter((item:any) =>
+    item.name.toLowerCase().includes(this.searchText.toLowerCase())
+  );
+}
 }
